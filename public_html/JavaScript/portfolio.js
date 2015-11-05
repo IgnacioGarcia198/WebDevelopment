84dbf94e3e758bc7258ab9d386dc9627ea7e7bae
Nacho Garcia <nash.makineta@gmail.com>
2015-11-05 06:29:54 +0000

    - We make a commit at this point at this point to derive different paths. This branch's idea is almost working properly though it needs slight modifications, and it is high time we commit it and set out for a new branch.

--- public_html/JavaScript/portfolio.js
+++ public_html/JavaScript/portfolio.js
@@ -132,6 +132,20 @@ function randMinMax(min, max, npos) {
     }
 }
 
+//=====================================================================================================================
+// TO HAVE IN ACCOUNT: WE MUST CONVERT THIS THINGS TO BE ADAPTABLE TO MOBILE PHONES, USING PERCENTAGES OR WHATEVER.
+//=====================================================================================================================
+
+// OPERATION OF THE GAME IN THE PAGE:
+// YOU HAVE TO CATCH THE CIRCLES IN ORDER
+// YOU HAVE TO PUT THEM IN THE TARGET, WHICH SHOULD BE CENTERED BELOW THE LAST LINE OF CIRCLEDIVS.
+// WHEN DROPPING THE CIRCLEDIV ON IT, THE CIRCLE DRAGGED GOES TO ITS PLACE AND SHOWS THE CONCEALED TEXT.
+
+var radius = 130;
+/*
+ * 
+ * @type Function|portfolio_L138.Ball
+ */
 Ball = (function() {
     function Ball(radius, color, text, x, y, dx, dy,format, i) {
         this.center = {x: x, y: y};
@@ -141,6 +155,7 @@ Ball = (function() {
         this.dx = dx;
         this.dy = dy;
         this.switchMove = true;
+        this.docked = false;
         /*if(format) {
             this.dom = $('<div class="circle2"><div '+ 'class="'+format+'">'+text+'</div></div>').appendTo('#ground');
         }
@@ -240,7 +255,7 @@ function balls() {
     var boundH = $('#ground').height(); 
     var boundW = $('#ground').width();
     for(var i = 0; i < nballs; i ++) {
-        var radius = randMinMax(50, 200);
+        //var radius = 130;//randMinMax(50, 200);
         var x = randMinMax(radius, boundW - radius);
         var y = randMinMax(radius, boundH - radius);
         var dx = randMinMax(1, 4);
@@ -256,10 +271,13 @@ function balls() {
             //console.log($(this).radius);
             //console.log($(this).toString());
             //dragging
+            
             var ind = $(this).attr('data-i');
-            console.log($(this).attr('data-i'));
-            ballsarray[ind].stop();
-            dragging = ind;
+            if(!ballsarray[ind].docked) { 
+                console.log($(this).attr('data-i'));
+                ballsarray[ind].stop();
+                dragging = ind;
+            }
             //console.log(ballsarray[ind].radius);
         });
         bal.dom.on('mouseup', function(e) {
@@ -270,29 +288,30 @@ function balls() {
             //console.log($(this).toString());
             //dragging = false;
             var ind = $(this).attr('data-i');
-            console.log($(this).attr('data-i'));
-            var thisBall = ballsarray[ind];
+            if(!ballsarray[ind].docked) {
+                console.log($(this).attr('data-i'));
+                var thisBall = ballsarray[ind];
+                var parentOffset = $(this).parent().offset();
+                var x = e.pageX - parentOffset.left + thisBall.radius;
+                var y = e.pageY - parentOffset.top + thisBall.radius;
+                var bw = thisBall.boundWidth;
+                var bh = thisBall.boundHeight;
+                if(x - radius < 0) {
+                    x = radius;
+                } 
+                if(x + radius > bw) {
+                    x = bw - radius; 
+                }
+                if(y - radius < 0) {
+                    y = radius;
+                } 
+                if(y + radius > bh) {
+                    y = bh - radius; 
+                }
             
-            var parentOffset = $(this).parent().offset();
-            var x = e.pageX - parentOffset.left + thisBall.radius;
-            var y = e.pageY - parentOffset.top + thisBall.radius;
-            var bw = thisBall.boundWidth;
-            var bh = thisBall.boundHeight;
-            if(x - radius < 0) {
-                x = radius;
-            } 
-            if(x + radius > bw) {
-                x = bw - radius; 
+                thisBall.putCenterAt(x, y);
+                thisBall.start();
             }
-            if(y - radius < 0) {
-                y = radius;
-            } 
-            if(y + radius > bh) {
-                y = bh - radius; 
-            }
-            
-            thisBall.putCenterAt(x, y);
-            thisBall.start();
             //this.putCenterAt();
             //console.log(ballsarray[ind].radius);
         });
@@ -311,8 +330,10 @@ function balls() {
         ballsarray.push(bal);
         
     }
+    writeBallCells();
     //$(this,callback);
     loop();
+    
 }
 
 loop = function () {
@@ -330,9 +351,32 @@ var lines = [];
 function initAboutMe() {
     $( "#drop" ).droppable({
         drop: function( event, ui ) {
-            ballsarray[dragging].dom.find(':first-child').removeClass('oneline');
-            console.log(ballsarray[dragging].dom.find('.txt').html());
+            var thisBall = ballsarray[dragging];
+            
+            thisBall.dom.draggable("destroy");
+            thisBall.dom.find(':first-child').removeClass('oneline');
+            console.log(thisBall.dom.find('.txt').html());
+            // We check if "dragging" is 0 or ballsarray[dragging-1] is false.
+            // Meaning: We are dropping the next minimum number of div in the order.
+            /*if(dragging === false) {
+                console.log("dragging is false omg");
+            }
+            else if(dragging !== false) {
+                console.log("dragging is not false omg; dragging = " + dragging);
+                if(dragging !== 0) {
+                    console.log("dragging not equal to 0");
+                }
+            }*/
+            if((dragging !== false) && ((dragging == 0) || (ballsarray[dragging-1] === false))) {
+                console.log("will do it now");
+                carryDivToPlace(dragging);
+                console.log(ballCells);
+                thisBall.docked = true;
+                ballsarray[dragging] = false;
+            }
+            
             dragging = false;
+            
             //ballsarray[dragging].dom.('')
         }
     }); 
@@ -349,7 +393,7 @@ function initAboutMe() {
         }
     });*/
     circleTexts(balls);
-    
+    //writeBallCells();
     
     
     
@@ -368,4 +412,49 @@ function initAboutMe() {
         }
     });
 }*/
+
+var percPad = 0.1;
+function writeBallCells() {
+    var vpw = $(window).width();
+    //var vpw = $('#ground').width();
+    //ballsInARow = vpw/2.2/radius;//Math.floor(vpw*0.9/ballsarray.length);
+    
+    var wh = Math.round(radius*2*(1+percPad));
+    var ballsInARow = Math.floor(vpw/wh);
+    console.log('balls in a row: ' + ballsInARow);
+    var rows = Math.ceil(ballsarray.length/ballsInARow);
+    // HERE WE WRITE THE TABLE 
+    ballCells = $('#ground').find('#ballCells');
+    var ballCellsHtml = [];
+    for(var i = 0; i < rows; i ++) {
+        ballCellsHtml.push('<tr>');
+        for(var j = 0; j < ballsInARow; j ++) {
+            var txt = '<td class="empty" style="width: ' + wh + '; height: ' + wh + ';"></td>';
+            ballCellsHtml.push(txt);
+            console.log("writing td for ball");
+        }
+        ballCellsHtml.push('</tr>');
+    }
+    var txTable = ballCellsHtml.join("");
+    console.log("the table: \n" + txTable);
+    ballCells.html(txTable);
+    console.log("AND TABLE IS: \n" + ballCells);
+}
+
+function carryDivToPlace(ballIndex) {
+    // For this, the easiest could be to make as many tds and ths as calculated
+    // And then, we searh for td.empty:first inside the table, and each time we put a div in a cell we remove the class
+    // .empty  ==> Pretty easy man!!
+    //var ballCells = $('#ground').find('#ballCells');
+    console.log("TABLE BEFORE: \n" + ballCells.html());
+    var firstEmpty = ballCells.find('.empty').filter(':first');
+    firstEmpty.html(ballsarray[ballIndex].dom.firstChild()); 
+    console.log("should be td " + ballsarray[ballIndex].dom.parent());
+    
+    console.log("just put in table using index " + ballIndex + ": " + firstEmpty.html());
+    firstEmpty.removeClass("empty");
+    console.log("\n\nAnd it was put in td: " + firstEmpty.parent());
+    console.log("the new table: \n" + ballCells.html());
+    // Now the problem could be putting the enough padding It needs to be 10% or 20% of the radius.
+}
 
